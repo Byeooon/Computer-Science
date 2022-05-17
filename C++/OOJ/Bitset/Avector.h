4e69c9bb197bba004fd73a183d542cf09071febf
Byeooon <junhyeong0519@kookmin.ac.kr>
2022-05-17 17:27:35 +0900

    bitset header

---
+++ C++/OOJ/Bitset/Avector.h
@@ -0,0 +1,15 @@
+//Avector.h + cpp
+#include <iostream>
+#include "Kvector.h"
+using namespace std;
+
+#define N 3
+class Avector : public Kvector{
+    char table[N];
+public:
+    Avector(int sz=0, int v=0, const char *t="abc");
+    void setTable(const char *t);
+    void print() const;
+friend std::ostream& operator <<(std::ostream& os, const Avector& v);
+};
+
---
+++ C++/OOJ/Bitset/Bvector.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Kvector.h"
+using namespace std;
+
+class Bvector : public Kvector {
+protected:
+	unsigned int* u_m;
+	const int NBITS;
+public:
+	Bvector(int nbits = 32);
+	Bvector(const Bvector& e);
+	bool bit(int pos) const;
+	void set(int pos);
+	void reset(int pos);
+	int size() const { return NBITS; }
+	void clear() { for (int i = 0; i < len; i++) m[i] = 0; }
+	void print() const;
+
+// return the instance rather than the reference, hence immutable	
+bool operator[](int idx) const { return bit(idx); }
+Bvector& operator=(const Bvector& v);
+friend std::ostream& operator<<(std::ostream& os, const Bvector& v);
+};
